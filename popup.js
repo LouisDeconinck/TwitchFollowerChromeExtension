31d85f0bf34c776459b2c9941a99df1d90095489
Louis Deconinck <louis.dck@gmail.com>
2022-06-14 23:17:19 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,14 @@
-// When the start button is clicked, a counter is set to 0
+// Start button
 start.addEventListener("click", async () => {
-	chrome.runtime.sendMessage({ start: true });
+	chrome.runtime.sendMessage({ start: true, type: "start" });
+});
+
+// Read csv
+let loadButton = document.getElementById("load").addEventListener("click", () => {
+	Papa.parse(document.getElementById("csvFile").files[0], {
+		complete: function (results, file) {
+			console.log("Parsing complete:", results, file);
+			chrome.runtime.sendMessage({ csvlist: results, type: "list" });
+		},
+	});
 });
