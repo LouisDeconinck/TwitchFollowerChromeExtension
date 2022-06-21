fbd38b210f184a6c5ff29bb94fd40930164c7a4f
Louis Deconinck <louis.dck@gmail.com>
2022-06-21 19:51:24 +0200

    Update follow.js

--- follow.js
+++ follow.js
@@ -12,21 +12,26 @@ function checkButtons(timeout) {
 	return new Promise((resolve, reject) => {
 		var timer = false;
 		const observer = new MutationObserver(() => {
+			// If there's an unfollow button go to next link
 			if (document.querySelectorAll("button[data-a-target='unfollow-button']").length) {
 				observer.disconnect();
 				console.log("Unfollow button found.");
 				if (timer !== false) clearTimeout(timer);
 				return resolve();
 			}
+
+			// If there's a follow button click it
 			if (document.querySelectorAll("button[data-a-target='follow-button']").length) {
 				console.log("Follow button found");
-				setTimeout(clickFollow, 1000);
+				setTimeout(clickFollow, 1500);
 			}
 		});
+
 		observer.observe(document.body, {
 			childList: true,
 			subtree: true,
 		});
+
 		if (timeout)
 			timer = setTimeout(() => {
 				observer.disconnect();
@@ -35,7 +40,7 @@ function checkButtons(timeout) {
 	});
 }
 
-checkButtons(5000)
+checkButtons(8000)
 	.then(function () {
 		console.log("Button found.");
 	})
